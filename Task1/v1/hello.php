4c400b873f70d23e9a0accef36876dabb1617e41
abonguez <52037099+abonguez@users.noreply.github.com>
2021-01-20 16:50:40 -0400

    Tarea 1 Parte 1
    Los zip folders tienen todos los proyectos del ejercicio.

---
+++ Task1/v1/hello.php
@@ -0,0 +1,20 @@
+﻿<?php
+
+//Hola Mundo: Este servicio solo devuelve "world" cuando lo invocan
+//Se le pondrá aprueba con dos metodologías de invocación:
+// 1. Un DOCTYPE HTML
+// 2. Un ejecutable con UI/UX
+//NOTA: Estas pruebas se harán en una plataforma HTTPS
+//		En este proyecto se está utilizando WAMP Server en localhost:443
+//		El servidor está corriendo SSL/TLS Con Certificados y Private.Keys
+//		creados con la herramienta OpenSSL. 
+//		El certificado es self-signed.
+
+// Josè M de Abongüez
+// ...www/v1/hello.php
+
+$HolaMundo = "world";
+
+echo $HolaMundo;// ////
+
+?>
