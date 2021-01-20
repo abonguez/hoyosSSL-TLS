f7040cd4a5c9b3cd504ae10aa393b14cc61bf976
abonguez <52037099+abonguez@users.noreply.github.com>
2021-01-20 16:47:30 -0400

    Tarea 1 Parte 1
    El sub folder llamado v1 tiene cada proyecto.

---
+++ Task1/CallHelloHTML.php
@@ -0,0 +1,19 @@
+﻿<!--
+- Esta archivo tiene como propósito invocar el
+  servicio de hello.php
+- Ambos archivos deben estar en el mismo nivel jerárquico.
+// Josè M de Abongüez
+// invoca a ...www/v1/hello.php
+-->
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<a Test $GET Hola Mundo</a>
+
+<?php require_once 'hello.php' ?>
+
+
+</body>
+</html>
