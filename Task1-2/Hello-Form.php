3c3d955f2d0429068444c1f003c07de3521f187c
abonguez <52037099+abonguez@users.noreply.github.com>
2021-01-27 18:42:03 -0400

    Add files via upload

---
+++ Task1-2/Hello-Form.php
@@ -0,0 +1,16 @@
+﻿<!DOCTYPE html>
+<html>
+<body>
+
+// TEST SSL/TLS
+
+<form action="hello.php" method="post">
+	<input type="text" name="data0" placeholder="D0">
+	<input type="text" name="data1" placeholder="D1">
+	<input type="submit">
+
+</form>
+
+
+</body>
+</html>
---
+++ Task1-2/hello.php
@@ -0,0 +1,31 @@
+﻿<?php
+
+//YOUR CODE GOES HERE
+//Proyecto para uso de $_POST y un form que pase los parámetros
+
+if (!isset($myStruct)){$myStruct = new stdClass();} //preven NULL
+
+$myStruct->data = "world";
+$myStruct->data2 = "user";
+
+$myJSON = json_encode($myStruct);
+
+//echo $myJSON;
+ 
+ if( (isset($_POST['data0'])) && (isset($_POST['data1'])) )
+  {
+	
+	$data0 = $_POST['data0'];
+  	$data1 = $_POST['data1'];
+  	echo "Enviastes D0 {$data0} D1 {$data1}";
+	
+	echo "\r\n".$myJSON;
+	
+	//echo "TEST";
+	
+  	
+ }
+ 
+//echo "test";
+
+?>
